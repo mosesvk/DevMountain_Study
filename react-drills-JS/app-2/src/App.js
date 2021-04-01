29abc7fa1a3793b666e52b027f220e33484e331b
Moses Kaumatule <kaumatule93@gmail.com>
2021-03-31 18:04:38 -0600

    update file transfer

---
+++ react-drills-JS/app-2/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import logo from './logo.svg';
+import './App.css';
+
+export default class App extends Component {
+  constructor(){
+    super(); 
+
+    this.state = {
+      foods: ['spaghetti', 'ice cream', 'sushi', 'bologna', 'cheese', 'ribs']
+    };
+  }
+
+  render(){
+    let foodsToDisplay = this.state.foods.map((elem, i) => {
+      return <h2 key={i}>{elem}</h2>;
+    });
+    return (
+      <div className="App"> {foodsToDisplay} </div>
+    );
+  }
+}
+
+
