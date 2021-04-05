6009a4ac6618c786247f3bf824c3849602e9ee73
Moses Kaumatule <kaumatule93@gmail.com>
2021-04-05 16:36:09 -0600

    complete postgresql LE

--- postgresql-db-basics/artist.sql
+++ postgresql-db-basics/artist.sql
@@ -1,4 +1,18 @@
 --Add 3 new artists to the artist table. ( It's already created )
-INSERT INTO artist ( name ) VALUES ( 'artist name' );
+INSERT INTO artist ( name ) 
+  VALUES 
+  ('Rascal Flatts'),
+  ('Thomas Rhett'),
+  ('Sam Hunt');
 
 --Select 10 artists in reverse alphabetical order.
+--SELECT * FROM artist ORDER BY name DESC LIMIT 10;
+
+--Select 5 artists in alphabetical order.
+--SELECT * FROM artist ORDER BY name ASC LIMIT 5;
+
+--Select all artists that start with the word 'Black'.
+SELECT * FROM artist WHERE name LIKE 'Black%';
+
+--Select all artists that contain the word 'Black'.
+SELECT * FROM artist WHERE name LIKE '%Black%';
