fb25854646cc5ae10aa0206f57d5b99a70c67a23
mosesvk <kaumatule93@gmail.com>
2022-03-09 19:20:33 -0700

    full stack interview

--- code_interview_beginner/src/App.js
+++ code_interview_beginner/src/App.js
@@ -1,19 +1,29 @@
+import { useState } from 'react';
 import axios from 'axios'
+
 import './App.css';
 
 const App = () => {
+  const [jsonData, setJsonData] = useState('')
 
   const getData = async() => {
-    axios.get('http://localhost:5000/api')
-      .then(res => {
-        console.log(res)
-      })
+    const apiData = await fetch('/api', {
+      method: 'GET',
+      headers: {
+        'content-type': 'application/json',
+        'Accept': 'application/json'
+      }
+    })
+      .then(res => res.json())
+    // console.log(apiData)
+    setJsonData(JSON.stringify(apiData, null, 2))
   }
 
 
   return (
     <div className="App">
       <button onClick={getData}>Connect to Backend</button>
+      <pre>{jsonData}</pre>
     </div>
   );
 }
