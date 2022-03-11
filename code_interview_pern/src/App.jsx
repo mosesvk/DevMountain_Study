58e973172135e53c779eeb6f4faebc053c76b0f9
mosesvk <kaumatule93@gmail.com>
2022-03-11 15:08:21 -0700

    pern app server

---
+++ code_interview_pern/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "./App.css";
+
+//components
+
+import InputTodo from "./components/InputTodo";
+import ListTodos from "./components/ListTodo";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <InputTodo />
+        <ListTodos />
+      </div>
+    </>
+  );
+}
+
+export default App;
