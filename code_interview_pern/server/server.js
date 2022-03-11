ef17e92610dc16a05319feb3f0e8d2198dc1516d
mosesvk <kaumatule93@gmail.com>
2022-03-11 14:37:11 -0700

    pern app server

--- code_interview_pern/server/server.js
+++ code_interview_pern/server/server.js
@@ -49,16 +49,41 @@ app.get('/todos/:id', async (req, res) => {
       id,
     ]);
 
-    res.json(todo.rows[0])
+    res.json(todo.rows[0]);
   } catch (err) {
     console.error(err.message);
   }
 });
 
 // update a todo
+app.put('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+
+    const updateTodo = await pool.query(
+      'UPDATE todo SET description = $1 WHERE todo_id = $2',
+      [description, id]
+    );
+
+    res.json({ message: 'Todo was updated', todo: updateTodo.rows[0] });
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 // delete a todo
+app.delete('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query('DELETE FROM todo WHERE todo_id = $1', [id]);
+    res.json('Todo was deleted');
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
+///////////////////////////////
 app.listen(PORT, () => {
   console.log(`server has started on port: ${PORT}`);
 });
