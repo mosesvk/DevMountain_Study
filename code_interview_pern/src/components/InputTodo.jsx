89f15d7bbb8395c1bc1d106ed2c4d0233de1a138
mosesvk <kaumatule93@gmail.com>
2022-03-12 20:51:08 -0700

    pern app server

--- code_interview_pern/src/components/InputTodo.jsx
+++ code_interview_pern/src/components/InputTodo.jsx
@@ -21,7 +21,7 @@ const InputTodo = () => {
 
   return (
     <>
-      <h1 className="text-center mt-5">Pern Todo List</h1>
+      <h1 className="text-center mt-5">TODO LIST</h1>
       <form className="d-flex mt-5" onSubmit={onSubmitForm}>
         <input
           type="text"
