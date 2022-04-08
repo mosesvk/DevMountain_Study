c0295b9da380b04524ec7e73773fbc527e55b9ce
mosesvk <kaumatule93@gmail.com>
2022-04-08 16:24:48 -0600

    nextjs project fetching data

--- next-tailwind-pokedex/pages/index.js
+++ next-tailwind-pokedex/pages/index.js
@@ -1,11 +1,23 @@
-import Layout from '../components/Layout'
+import Layout from '../components/Layout';
 
 export default function Home() {
   return (
     <Layout title='NextJS Pokedex'>
-      <h1 className='text-4xl mb-8 text-center'>
-        NextJS Pokedex
-      </h1>
+      <h1 className='text-4xl mb-8 text-center'>NextJS Pokedex</h1>
     </Layout>
-  )
+  );
+}
+
+export async function getStaticProps(context) {
+  try {
+    const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100');
+    const { results } = await res.json();
+    // the res is an object which includes the 'results' value. We only need the 'results' from the res object
+    
+  } catch (err) {
+    console.error(err);
+  }
+  return {
+    props: {},
+  };
 }
