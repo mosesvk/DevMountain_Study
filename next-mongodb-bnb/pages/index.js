42f61e073658437e727af99796b2030ef605cb26
mosesvk <kaumatule93@gmail.com>
2022-06-10 13:43:58 -0600

    adding a limit to calling back

--- next-mongodb-bnb/pages/index.js
+++ next-mongodb-bnb/pages/index.js
@@ -1,79 +1,20 @@
 import Head from 'next/head'
+import clientPromise from '../../lib/mongodb'
 
 export default function Home({ isConnected }) {
   return (
     <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js with MongoDB!</a>
-        </h1>
-
-        {isConnected ? (
-          <h2 className="subtitle">You are connected to MongoDB</h2>
-        ) : (
-          <h2 className="subtitle">
-            You are NOT connected to MongoDB. Check the <code>README.md</code>{' '}
-            for instructions.
-          </h2>
-        )}
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="/movies" className="card">
-            <h3>Movies &rarr;</h3>
-            <p>mongo_mflix</p>
-          </a>
-
-          <a href="/listings" className="card">
-            <h3>Listings &rarr;</h3>
-            <p>mongodb_airbnb</p>
-          </a>
-
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
 
     </div>
   )
 }
 
 export async function getServerSideProps(context) {
-  try {
-    await clientPromise
-    // `await clientPromise` will use the default database passed in the MONGODB_URI
-    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
-    //
-    // `const client = await clientPromise`
-    // `const db = client.db("myDatabase")`
-    //
-    // Then you can execute queries against your database like so:
-    // db.find({}) or any of the MongoDB Node Driver commands
 
-    return {
-      props: { isConnected: true },
-    }
-  } catch (e) {
-    console.error(e)
-    return {
-      props: { isConnected: false },
-    }
+  
+
+  return {
+    props: { }
   }
 }
