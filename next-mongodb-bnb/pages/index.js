3f2c8b57461e97a2598b86d7bd666f46714f369f
mosesvk <kaumatule93@gmail.com>
2022-06-10 22:56:10 -0600

    adding ui

--- next-mongodb-bnb/pages/index.js
+++ next-mongodb-bnb/pages/index.js
@@ -1,28 +1,84 @@
-import clientPromise from '../lib/mongodb'
+import Head from 'next/head';
+import clientPromise from '../lib/mongodb';
 
 export default function Home({ properties }) {
-
   console.log(properties)
-
   return (
-    <div className="container">
+    <>
+      <Head>
+        <title>NextJS & MongoDB</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
 
+      <div class='mx-auto container'>
+        <div class='flex'>
+          <div class='row w-full text-center my-4'>
+            <h1 class='text-4xl font-bold mb-5'>MoBnb</h1>
+          </div>
+        </div>
+      </div>
 
-    </div>
-  )
+      <div class='flex flex-row flex-wrap'>
+        {properties &&
+          properties.map((property) => (
+            <div>
+              <div class='flex-auto w-1/4 rounded overflow-hidden shadow-lg m-2'>
+                <img src={property.image} class='w-full' />
+              </div>
+
+              <div class='px-6 py-4'>
+                <div class='font-bold text-xl mb-2'>{property.name} (up to {property.guests} guests)</div>
+                <p>{property.address.street}</p>
+                <p class='text-gray-700 text-base'>{property.summary}</p>
+              </div>
+
+              <div class='text-center py-2 my-2 font-bold'>
+                <span class='text-green-500'>${property.price}</span>/night
+              </div>
+
+              <div class='text-center py-2 my-2'>
+                <button class='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-5 rounded'>Book</button>
+              </div>
+
+            </div>
+          ))}
+      </div>
+    </>
+  );
 }
 
 export async function getServerSideProps(context) {
-
   const client = await clientPromise;
 
-  const db = client.db('sample_airbnb')
+  const db = client.db('sample_airbnb');
 
-  let data = await db.collection('listingsAndReviews').find({}).limit(10).toArray()
+  let data = await db
+    .collection('listingsAndReviews')
+    .find({})
+    .limit(10)
+    .toArray();
 
-  let properties = JSON.parse(JSON.stringify(data))
+  // If I wanted to add only specific data points with the id of 1...
+  // you would add ".project({ _id: 1 })"
+  // You could do this with any field or parameter
+
+  const properties = JSON.parse(JSON.stringify(data));
+
+  const filtered = properties.map((property) => {
+
+    const price = JSON.parse(JSON.stringify(property.price))
+
+    return { //props
+      _id: property._id, 
+      name: property.name,
+      image: property.images.picture_url,
+      address: property.address,
+      summary: property.summary,
+      price: price.$numberDecimal,
+    }
+  })
 
   return {
-    props: { properties }
-  }
+    props: { properties: filtered },
+  };
 }
