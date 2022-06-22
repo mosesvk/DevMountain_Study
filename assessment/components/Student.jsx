62c768a2ea76d47b30dc20ba1444d7c52eb149a3
mosesvk <kaumatule93@gmail.com>
2022-06-22 07:40:35 -0600

    assessment project

--- assessment/components/Student.jsx
+++ assessment/components/Student.jsx
@@ -1,11 +1,22 @@
-
-const Student = () => {
-
+const Student = ({ item }) => {
   return (
-    <>
+    <div class='flex mb-2 border-bottom'>
+      <div class='w-1/3'>
+        <div class='rounded-full border-black'>
+          <img src={item.pic} />
+        </div>
+      </div>
+      <div class='w-2/3'>
+        <h1>
+          {item.firstName} {item.lastName}
+        </h1>
+        <p>Email: {item.email}</p>
+        <p>Company: {item.company}</p>
+        <p>Skill: {item.skill}</p>
+        <p></p>
+      </div>
+    </div>
+  );
+};
 
-    </>
-  )
-}
-
-export default Student
+export default Student;
