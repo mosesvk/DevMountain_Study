4511b3b997d753900dfcac6453f8e9259260088b
mosesvk <kaumatule93@gmail.com>
2022-06-22 11:58:38 -0600

    assessment project

--- assessment/components/Student.jsx
+++ assessment/components/Student.jsx
@@ -1,19 +1,21 @@
 const Student = ({ item }) => {
   return (
-    <div class='flex mb-2 border-bottom'>
-      <div class='mr-5'>
+    <div class='flex py-2 border-bottom'>
+      <div class='m-5'>
         <div class='rounded-full border-2'>
-          <img src={item.pic} class='w-24' />
+          <img src={item.pic} class='w-24 rounded-full' />
         </div>
       </div>
-      <div class='w-2/3'>
-        <h1>
+      <div class='w-2/3 ml-5'>
+        <h1 class='text-3xl font-bold uppercase mb-2'>
           {item.firstName} {item.lastName}
         </h1>
-        <p>Email: {item.email}</p>
-        <p>Company: {item.company}</p>
-        <p>Skill: {item.skill}</p>
-        <p></p>
+        <div class='ml-5'>
+          <p>Email: {item.email}</p>
+          <p>Company: {item.company}</p>
+          <p>Skill: {item.skill}</p>
+          <p>Average</p>
+        </div>
       </div>
     </div>
   );
