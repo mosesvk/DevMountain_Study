be3927d53d5ea829898e8ed88c2862602b108926
mosesvk <kaumatule93@gmail.com>
2022-07-03 23:49:50 -0600

    added new react coding challenges folder from linkedIn learning

--- react-coding-challenges-linkedIn-learning/01-color-renderer/src/components/ColorRenderer.js
+++ react-coding-challenges-linkedIn-learning/01-color-renderer/src/components/ColorRenderer.js
@@ -19,9 +19,8 @@ const colors = [{
 
 export default function ColorRenderer () {
   return (
-    <h2>
-      Use the Color component to render each
-       item in the colors array on the page!
-    </h2>
+    colors.map((color, idx) => (
+      <Color key={idx} hex={color.hex} name={color.name} />
+    ))
   )
 }
