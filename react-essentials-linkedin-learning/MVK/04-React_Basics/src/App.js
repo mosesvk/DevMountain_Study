e269e6d44461b24b1314f163b162445deda194ee
mosesvk <kaumatule93@gmail.com>
2022-07-07 11:53:59 -0600

    update

--- react-essentials-linkedin-learning/MVK/04-React_Basics/src/App.js
+++ react-essentials-linkedin-learning/MVK/04-React_Basics/src/App.js
@@ -3,10 +3,18 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [emotion, setEmotion] = useState("happy");
+  const [secondaryEmotion, setSecondaryEmotion] = useState("tired")
 
   useEffect(() => {
-    console.log(`It's ${emotion} around here!`);
+    console.log(`Main Emotion is ${emotion}`);
   }, [emotion]);
+
+  useEffect(() => {
+    console.log(`Secondary Emotion is ${secondaryEmotion}`)
+  }, [secondaryEmotion])
+
+  // Dependency arrays are critical in efficiency with your app.
+  // Notice that we will only get a console log when certain variables are being affected. 
 
   return (
     <div className="App">
@@ -19,6 +27,14 @@ function App() {
       >
         Excited
       </button>
+
+      <h2>Secondary Emotion is {secondaryEmotion}</h2>
+      <button
+        onClick={() => setSecondaryEmotion('stressed')}
+      >Stressed</button>
+      <button
+        onClick={() => setSecondaryEmotion('calm')}
+      >Calm</button>
     </div>
   );
 }
