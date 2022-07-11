0f63ba742957a6cd57071261d021031c6088b471
mosesvk <kaumatule93@gmail.com>
2022-07-11 14:23:30 -0600

    update

--- react-essentials-linkedin-learning/MVK/06-Asynchronous-Code/Fetch_graphql/src/App.js
+++ react-essentials-linkedin-learning/MVK/06-Asynchronous-Code/Fetch_graphql/src/App.js
@@ -16,29 +16,47 @@ const opts = {
   body: JSON.stringify({ query })
 };
 
+function Lift({ name, elevationGain, status }) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>
+        {elevationGain} {status}
+      </p>
+    </div>
+  );
+}
+
 const App = () => {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null)
   const [loading, setLoading] = useState(false)
 
   useEffect(() => {
-    fetch(`https://snowtooth.moonhighway.com/`)
+    setLoading(true)
+    fetch(`https://snowtooth.moonhighway.com/`, opts)
       .then((res) => res.json())
       .then(setData)
       .then(() => setLoading(false))
       .catch(setError)
   }, []);
 
-  console.log(data)
-
   if (loading) return <h1>Loading...</h1>
   if (error) return <pre>{JSON.stringify(error)}</pre>
   if (!data) return null
 
+  console.log(data.data.allLifts)
+
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div>
+      {data.data.allLifts.map((lift) => (
+        <Lift
+          name={lift.name}
+          elevationGain={lift.elevationGain}
+          status={lift.status}
+        />
+      ))}
+    </div>
   );
 };
 
