740922c518d80fab329e77016a675b8f77204f21
mosesvk <kaumatule93@gmail.com>
2022-07-12 11:04:13 -0600

    getting started

--- react-linkedin_learning_interfaces/src/App.js
+++ react-linkedin_learning_interfaces/src/App.js
@@ -1,13 +1,40 @@
-import { BiArchive } from 'react-icons/bi';
+import { BiCalendar, BiTrash } from 'react-icons/bi';
+import AddAppointment from './components/AddAppointment'
+import Search from './components/Search';
+import appointments from './data.json'
 
 function App() {
   return (
-    <div className='container mx-auto mt-3 font-thin'>
-      <h1 className='text-5xl'>
-        {' '}
-        <BiArchive className='inline-block text-red-400' /> My App
-      </h1>
-    </div>
+    <>
+      <div className='container mx-auto mt-3 font-thin'>
+        <h1 className='text-5xl mb-3'>
+          {' '}
+          <BiCalendar className='inline-block text-red-400' /> My App
+        </h1>
+        <AddAppointment />
+        <Search />
+
+        <ul className='divide-y divide-gray-200'>
+          {appointments.map((appointment) => (
+            <li className="px-3 py-3 flex items-start">
+              <button type="button"
+                className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+                <BiTrash /></button>
+              <div className="flex-grow">
+                <div className="flex items-center">
+                  <span className="flex-none font-medium text-2xl text-blue-500">{appointment.petName}</span>
+                  <span className="flex-grow text-right">{appointment.aptDate}</span>
+                </div>
+                <div><b className="font-bold text-blue-500">Owner:</b> {appointment.ownerName}</div>
+                <div className="leading-tight">{appointment.aptNotes}</div>
+              </div>
+            </li>
+          ))}
+        </ul>
+        
+      </div>
+
+    </>
   );
 }
 
