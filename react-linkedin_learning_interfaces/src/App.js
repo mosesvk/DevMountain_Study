2b7ee6604bb10e0dbb54eb1e527ea4d9fbe796b8
mosesvk <kaumatule93@gmail.com>
2022-07-12 23:03:10 -0600

    react_interfaces useState hooks

--- react-linkedin_learning_interfaces/src/App.js
+++ react-linkedin_learning_interfaces/src/App.js
@@ -1,21 +1,21 @@
-import {useState, useEffect, useCallback} from 'react'
+import { useState, useEffect, useCallback } from 'react';
 import { BiCalendar } from 'react-icons/bi';
 import AddAppointment from './components/AddAppointment';
 import AppointmentInfo from './components/AppointmentInfo';
 import Search from './components/Search';
 
 function App() {
-  const [appointmentList, setAppointmentList] = useState([])
+  const [appointmentList, setAppointmentList] = useState([]);
 
   const fetchData = useCallback(() => {
     fetch(`./data.json`)
       .then((res) => res.json())
-      .then((data) => setAppointmentList(data))
-  }, [])
+      .then((data) => setAppointmentList(data));
+  }, []);
 
   useEffect(() => {
-    fetchData()
-  }, [fetchData])
+    fetchData();
+  }, [fetchData]);
 
   return (
     <>
@@ -29,7 +29,17 @@ function App() {
 
         <ul className='divide-y divide-gray-200'>
           {appointmentList.map((appointment) => (
-            <AppointmentInfo key={appointment.id} appointment={appointment} />
+            <AppointmentInfo
+              key={appointment.id}
+              appointment={appointment}
+              onDeleteAppointment={(appointmentId) =>
+                setAppointmentList(
+                  appointmentList.filter(
+                    (appointment) => appointment.id !== appointmentId
+                  )
+                )
+              }
+            />
           ))}
         </ul>
       </div>
