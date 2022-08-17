afb4f642fcdfb516813f7c1b5fbd6f967091801d
mosesvk <kaumatule93@gmail.com>
2022-08-17 04:34:33 -0600

    update

--- vanila-beginner-projects/2-drum-kit/index.js
+++ vanila-beginner-projects/2-drum-kit/index.js
@@ -1,1 +1,11 @@
-console.log('hello mayne')
+const buttons = document.querySelectorAll(".drum")
+
+let w_audio = new Audio('/sounds/tom-1.mp3')
+
+
+buttons.forEach(item => item.addEventListener('click', () => {
+  console.log(`${item.innerHTML} button got clicked`);
+  w_audio.play();
+}))
+
+
