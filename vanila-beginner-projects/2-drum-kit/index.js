64c1d960db3d327ca489afa4f1575efaebfb81d2
mosesvk <kaumatule93@gmail.com>
2022-08-17 18:03:02 -0600

    update

--- vanila-beginner-projects/2-drum-kit/index.js
+++ vanila-beginner-projects/2-drum-kit/index.js
@@ -9,14 +9,44 @@ let k_audio = new Audio('/sounds/crash.mp3')
 let l_audio = new Audio('/sounds/kick-bass.mp3')
 
 buttons.forEach(item => item.addEventListener('click', () => {
-  let letter = item.innerHTML
-  if (letter == 'w') w_audio.play();
-  if (letter == 'a') a_audio.play();
-  if (letter == 's') s_audio.play();
-  if (letter == 'd') d_audio.play();
-  if (letter == 'j') j_audio.play();
-  if (letter == 'k') k_audio.play();
-  if (letter == 'l') l_audio.play();
+  let letterClick = item.innerHTML
+
+  makeSound(letterClick)
 }))
+
+document.addEventListener('keydown', (e) => {
+  let letterPress = e.key
+  
+  makeSound(letterPress)
+})
+
+const makeSound = (key) => {
+
+  switch(key) {
+    case 'w':
+      w_audio.play()
+      break;
+    case 'a':
+      a_audio.play()
+      break;
+    case 's':
+      s_audio.play()
+      break;
+    case 'd':
+      d_audio.play()
+      break;
+    case 'j':
+      j_audio.play()
+      break;
+    case 'k':
+      k_audio.play()
+      break;
+    case 'l':
+      l_audio.play()
+      break;
+    default:
+      console.log(key)
+  }
+}
 
 
