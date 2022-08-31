3f56e91a18037c1a7a2edb51a34150051d054453
mosesvk <kaumatule93@gmail.com>
2022-08-30 22:28:41 -0600

    update README page!

--- next-firebase-evernote/pages/components/NoteOperations.jsx
+++ next-firebase-evernote/pages/components/NoteOperations.jsx
@@ -1,19 +1,33 @@
-import {useState} from 'react'
+import { useState } from 'react';
 import styles from '../../styles/Evernote.module.scss';
 
 const NoteOperations = () => {
   const [isInputVisible, setInputVisible] = useState(false);
+  const [noteTitle, setNoteTitle] = useState('');
 
+  const inputToggle = () => {
+    setInputVisible(!isInputVisible);
+  };
 
   return (
     <>
       <div className={styles.btnContainer}>
-        <button className={styles.button}>Add a New Note</button>
+        <button onClick={inputToggle} className={styles.button}>
+          Add a New Note
+        </button>
       </div>
 
-      <div className={styles.inputContainer}>
-        <input placeholder='Enter the Title..' className={styles.input} />
-      </div>
+      {isInputVisible ? (
+        <div className={styles.inputContainer}>
+          <input
+            placeholder='Enter the Title..'
+            className={styles.input}
+            onChange={(e) => setNoteTitle(e.target.value)}
+          />
+        </div>
+      ) : (
+        <></>
+      )}
     </>
   );
 };
