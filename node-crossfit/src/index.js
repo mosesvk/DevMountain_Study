853d0091942e2551c9f1c2dc16226225be862d9d
mosesvk <kaumatule93@gmail.com>
2022-10-05 14:38:55 -0600

    push

--- node-crossfit/src/index.js
+++ node-crossfit/src/index.js
@@ -4,10 +4,10 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 // testing
-app.get('/', () => {
+app.get('/', (req, res) => {
   res.send('<h2>Its Working!</h2>')
 })
 
 app.listen(PORT, () => {
-  console.log('API is listening on ${PORT}')
+  console.log(`API is listening on ${PORT}`)
 })
