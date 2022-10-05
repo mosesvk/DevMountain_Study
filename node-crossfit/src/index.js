45a2eafa4e96de762cb4b9f912a437a795bde63b
mosesvk <kaumatule93@gmail.com>
2022-10-05 15:56:49 -0600

    push

--- node-crossfit/src/index.js
+++ node-crossfit/src/index.js
@@ -1,12 +1,11 @@
 const express = require('express')
+const v1Router = require('./v1/routes')
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
-// testing
-app.get('/', (req, res) => {
-  res.send('<h2>Its Working!</h2>')
-})
+// connecting to our router in 'v1/routes/index.js
+app.use('/api/v1', v1Router)
 
 app.listen(PORT, () => {
   console.log(`API is listening on ${PORT}`)
