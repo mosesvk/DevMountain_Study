3f04a985bc5fbf778c2ab0aafd1ad31d3f868afa
mosesvk <kaumatule93@gmail.com>
2022-10-06 16:55:23 -0600

    error handling: workoutController, createNewWorkout

--- node-crossfit/src/services/workoutService.js
+++ node-crossfit/src/services/workoutService.js
@@ -1,42 +1,38 @@
 // The whole business logic will be in the SERVICE Layer that exports certain services (methods) which are used by the controller.
 
-
-
-
-const {v4: uuid} = require('uuid')
-const Workout = require("../database/Workout");
-
-
+const { v4: uuid } = require('uuid');
+const Workout = require('../database/Workout');
 
 const getAll = () => {
-
   const allWorkouts = Workout.getAllWorkouts();
 
   return allWorkouts;
 };
 
 const getOne = (workoutId, params) => {
+  console.log('getOneService');
+  console.log(params);
+  console.log('-----');
 
-  console.log('getOneService')
-  console.log(params)
-  console.log('-----')
+  const workout = Workout.getOneWorkout(workoutId);
 
-  const workout = Workout.getOneWorkout(workoutId)
-
-  return workout
+  return workout;
 };
 
 const createNew = (newWorkout) => {
   const workoutToInsert = {
     ...newWorkout,
     id: uuid(),
-    createdAt: new Date().toLocaleString('en-us', {timeZone: 'UTC'}),
-    updatedAt: new Date().toLocaleString('en-us', {timeZone: 'UTC'})
+    createdAt: new Date().toLocaleString('en-us', { timeZone: 'UTC' }),
+    updatedAt: new Date().toLocaleString('en-us', { timeZone: 'UTC' }),
+  };
+
+  try {
+    const createdWorkout = Workout.createNewWorkout(workoutToInsert);
+    return createdWorkout;
+  } catch (error) {
+    throw new Error({ status: 500, message: error?.message || error });
   }
-
-  const createdWorkout = Workout.createNewWorkout(workoutToInsert)
-
-  return createdWorkout
 };
 
 const updateOne = (workoutId, changes) => {
