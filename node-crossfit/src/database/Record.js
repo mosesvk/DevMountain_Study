6f0f19f02ac9763bc0137caf709c38c96bd8f8c8
mosesvk <kaumatule93@gmail.com>
2022-10-07 09:53:29 -0600

    Record.js -> db.js

--- node-crossfit/src/database/Record.js
+++ node-crossfit/src/database/Record.js
@@ -2,7 +2,14 @@ const DB = require('./db.json')
 
 const getRecordForWorkout = (workoutId) => {
   try {
-    
+    const record = DB.records.filter((record) => record.workout === workoutId)
+    if (!record) {
+      throw new Error({
+        status: 400,
+        message: `Can't find workout with the id '${workoutId}'`,
+      })
+    }
+    return record
   } catch (err) {
     throw new Error({ 
       status: err?.status || 500, 
