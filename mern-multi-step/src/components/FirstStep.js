ff88d565a276f36c7ed3584ba2bd71418c2902a8
mosesvk <kaumatule93@gmail.com>
2022-10-08 23:07:41 -0600

    initial mern project push

---
+++ mern-multi-step/src/components/FirstStep.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FirstStep = () => {
+  return (
+    <div>First Step Form</div>
+  )
+}
+
+export default FirstStep
---
+++ mern-multi-step/src/router/AppRouter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import FirstStep from '../components/FirstStep'
+import Header from '../components/Header'
+
+const AppRouter = () => {
+  return (
+    <div>AppRouter</div>
+  )
+}
+
+export default AppRouter
