312319829fcf492f7542949239eb491045130656
mosesvk <kaumatule93@gmail.com>
2022-10-16 13:17:44 -0600

    update

---
+++ react-mern/backend/index.js
@@ -0,0 +1,25 @@
+import app from './server'
+import mongodb from 'mongodb'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const MongoClient = mongodb.MongoClient
+
+const port = process.env.PORT || 4000
+
+MongoClient.connect(
+  process.env.REST_REVIEWS_DB_URI,
+  {
+    poolSize: 50,
+    wtimeout: 250,
+    useNewUrlParse: true
+  }
+).catch(err => {
+  console.error(err.stack)
+  process.exit(1)
+}).then(async (client) => {
+  app.listen(port, () => {
+    console.log(`Listening on Port $`)
+  })
+})
