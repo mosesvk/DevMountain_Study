1f4963d5b680b88d106f63ad40ad7cc1c8c3ca9e
mosesvk <kaumatule93@gmail.com>
2022-10-18 20:54:05 -0600

    update

--- react-mern/backend/controllers/projectControllers.ts
+++ react-mern/backend/controllers/projectControllers.ts
@@ -1,50 +1,96 @@
-import {Response, Request} from 'express'
+import { Response, Request } from 'express';
+const asyncHandler = require('express-async-handler');
+const mongoose = require('mongoose')
+
+const Project = require('../models/projectModel');
 
 //@desc Get All Projects
 //@route GET  /api/Projects
 //@access Public
-const getAllProjects = (req: Request, res: Response) => {
-  res.status(200).json({message: 'Get All Projects'})
-}
+const getAllProjects = asyncHandler(async (req: Request, res: Response) => {
+  try {
+    const projects = await Project.find();
+    res.status(200).json(projects);
+  } catch (err) {
+    res.status(400);
+    throw new Error('No Projects found');
+  }
+});
 
 //@desc Create Project
 //@route POST  /api/Projects/:id
 //@access Private
-const createProject = (req: Request, res: Response) => {
-  const title = req.body.title
-
-  if (!title) {
-    res.status(400)
-    throw new Error('Title is Required')
+const createProject = asyncHandler(async (req: Request, res: Response) => {
+  if (!req.body.title) {
+    res.status(400);
+    throw new Error('Title is required');
   }
-  res.json(req. body)
-}
+  const project = await Project.create(req.body);
+  if (!project) {
+    res.status(400);
+    throw new Error('Project not created');
+  }
+  res.status(201).json(project);
+});
 
 //@desc Get Single Project by id
 //@route GET  /api/Projects/:id
 //@access Public
-const getSingleProject = (req: Request, res: Response) => {
-  res.json({message: `GET Project - ${req.params.id}`})
-}
+const getSingleProject = asyncHandler(async (req: Request, res: Response) => {
+  console.log(req.params);
+  const project = await Project.findById(req.params.id);
+  if (!project) {
+    res.status(404);
+    throw new Error('Project id not found');
+  }
+  res.status(200).json(project);
+});
 
 //@desc Delete Project by id
 //@route DELETE  /api/Projects/:id
 //@access Private
-const deleteProject = (req: Request, res: Response) => {
-  res.json({message: `DELETE Project - ${req.params.id}`})
-}
+const deleteProject = asyncHandler(async (req: Request, res: Response) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        res.status(400);
+        throw new Error(`${req.params.id} is not a valid id`);
+    }
+    const project = await Project.findByIdAndDelete(req.params.id);
+    if (!project) {
+        res.status(404);
+        throw new Error('Project not found');
+    }
+    res.status(200).json({
+        message: `Project ${req.params.id} deleted`,
+        project: project,
+    });
+});
 
 //@desc Update Project by id
 //@route GET  /api/Projects/:id
 //@access Private
-const updateProject = (req: Request, res: Response) => {
-  res.json({message: `UPDATE Project - ${req.params.id}`})
-}
+const updateProject = asyncHandler(async (req: Request, res: Response) => {
+  if (!req.body.title) {
+    res.status(400);
+    throw new Error('Title is required');
+  }
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    res.status(400);
+    throw new Error(`${req.params.id} is not a valid id`);
+  }
+  const project = await Project.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  if (!project) {
+    res.status(404);
+    throw new Error('Project not found');
+  }
+  res.json(project);
+});
 
 module.exports = {
   getAllProjects,
   getSingleProject,
   createProject,
   deleteProject,
-  updateProject
-}
+  updateProject,
+};
--- react-mern/backend/routes/projectRoutes.ts
+++ react-mern/backend/routes/projectRoutes.ts
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = express.Router();
+
 const {
   getAllProjects,
   getSingleProject,
