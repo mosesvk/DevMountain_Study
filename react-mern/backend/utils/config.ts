0f7d1ea4a9dfb6783dcc1a101c8cd449cd155131
mosesvk <kaumatule93@gmail.com>
2022-10-21 16:41:52 -0600

    update

--- react-mern/backend/utils/config.ts
+++
@@ -1,5 +0,0 @@
-const dotenv = require('dotenv').config()
-
-export const port = process.env.PORT || 8000
-export const node_env = process.env.NODE_ENV || 'development'
-export const mongo_uri = process.env.MONGO_URI || null
