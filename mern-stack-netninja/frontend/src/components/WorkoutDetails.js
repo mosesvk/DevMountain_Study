222b805eef8b02e360de93aecf98769b8e84e88b
mosesvk <kaumatule93@gmail.com>
2022-10-22 12:29:02 -0600

    starting on the frontend

---
+++ mern-stack-netninja/frontend/src/components/WorkoutDetails.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const WorkoutDetails = ({workout}) => {
+  return (
+    <div className='workoout-details'>
+      <h4>{workout.title}</h4>
+      <p><strong>Load (lbs.): </strong>{workout.load}</p>
+      <p><strong>Reps: </strong>{workout.reps}</p>
+      <p>{workout.createdAt}</p>
+    </div>
+  )
+}
+
+export default WorkoutDetails
