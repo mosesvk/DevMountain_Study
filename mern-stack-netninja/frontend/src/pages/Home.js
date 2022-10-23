96eb2175d38d7019c4bb1326f1b11595bb29ac65
mosesvk <kaumatule93@gmail.com>
2022-10-22 19:39:35 -0600

    starting on the frontend

--- mern-stack-netninja/frontend/src/pages/Home.js
+++ mern-stack-netninja/frontend/src/pages/Home.js
@@ -1,33 +1,35 @@
-import { useEffect, useState } from 'react';
-import WorkoutDetails from '../components/WorkoutDetails';
-import WorkoutForm from '../components/WorkoutForm';
+import { useEffect, useState } from "react"
+
+// components
+import WorkoutDetails from "../components/WorkoutDetails"
+import WorkoutForm from "../components/WorkoutForm"
 
 const Home = () => {
-  const [workouts, setWorkouts] = useState(null);
+  const [workouts, setWorkouts] = useState(null)
 
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch('/api/workouts');
-      const data = await response.json();
+      const response = await fetch('/api/workouts')
+      const json = await response.json()
 
       if (response.ok) {
-        setWorkouts(data);
+        setWorkouts(json)
       }
-    };
+    }
 
-    fetchWorkouts();
-  }, []);
+    fetchWorkouts()
+  }, [])
 
   return (
-    <div className='home'>
-      <div className='workouts'>
-        {workouts?.map((workout) => (
+    <div className="home">
+      <div className="workouts">
+        {workouts && workouts.map(workout => (
           <WorkoutDetails workout={workout} key={workout._id} />
         ))}
       </div>
       <WorkoutForm />
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
