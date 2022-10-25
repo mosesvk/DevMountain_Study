ee267ed43f66f9e730be1bd4e3f3841ad398dee2
mosesvk <kaumatule93@gmail.com>
2022-10-25 14:25:59 -0600

    useLogout frontend hook and context

--- mern-stack-netninja/frontend/src/pages/Login.js
+++ mern-stack-netninja/frontend/src/pages/Login.js
@@ -1,35 +1,39 @@
-import { useState } from "react"
+import { useState } from 'react';
+import { useLogin } from '../hooks/useLogin';
 
 const Login = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { login, error, isLoading } = useLogin();
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
-    console.log(email, password)
-  }
+    await login(email, password);
+  };
 
   return (
-    <form className="login" onSubmit={handleSubmit}>
+    <form className='login' onSubmit={handleSubmit}>
       <h3>Log In</h3>
-      
+
       <label>Email address:</label>
-      <input 
-        type="email" 
-        onChange={(e) => setEmail(e.target.value)} 
-        value={email} 
+      <input
+        type='email'
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
       />
       <label>Password:</label>
-      <input 
-        type="password" 
-        onChange={(e) => setPassword(e.target.value)} 
-        value={password} 
+      <input
+        type='password'
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
       />
 
-      <button>Log in</button>
+      <button disabled={isLoading}>Log in</button>
+      {error && <div className='error'>{error}</div>}
     </form>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
