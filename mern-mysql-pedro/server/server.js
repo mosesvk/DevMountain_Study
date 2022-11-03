5ab7c278e1ae088db826e1a9f5baa8a442ef88ca
mosesvk <kaumatule93@gmail.com>
2022-11-03 02:39:12 -0600

    update

--- mern-mysql-pedro/server/server.js
+++ mern-mysql-pedro/server/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+var colors = require('colors');
 
-const PORT = process.env.PORT || 8000
+const db = require('./models');
 
-app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`))
+const PORT = process.env.PORT || 8000;
+
+db.sequelize.sync().then(() => {
+  console.log(colors.bgMagenta('Connected to DB'));
+  app.listen(PORT, () => console.log(colors.bgBlue(`Listening on PORT ${PORT}`)));
+});
