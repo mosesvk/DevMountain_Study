c87b81cf7a60d442e073611e539fe71997274006
mosesvk <kaumatule93@gmail.com>
2022-11-10 20:23:46 -0700

    update POSTS section

--- microservice-blog/posts/index.js
+++ microservice-blog/posts/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {randombytes} = require('crypto') // generates new id that is assigned to the post that the user is going to create
+const {randomBytes} = require('crypto') // generates new id that is assigned to the post that the user is going to create
 require('dotenv').config(); 
 var bodyParser = require('body-parser')
 
@@ -9,12 +9,12 @@ app.use(bodyParser.json())
 
 const posts = {}
 
-app.get('/posts', (req, res) => {
+app.get('/api/posts', (req, res) => {
   res.status(500).send(posts)
 })
 
-app.post('/posts', (req, res) => {
-  const id = randombytes(4).toString('hex');
+app.post('/api/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex');
   const {title} = req.body
 
   posts[id] = {
