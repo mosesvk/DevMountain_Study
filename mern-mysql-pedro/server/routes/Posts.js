ca7bf8bc6323bc2c900bef5366051c7088efb131
mosesvk <kaumatule93@gmail.com>
2022-11-17 18:36:10 -0700

    update

--- mern-mysql-pedro/server/routes/Posts.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const { getAllPosts, createPost } = require('../controllers/PostCtrl');
-
-// router.get('/', getAllPosts)
-app.route('/').get(getAllPosts).post(createPost);
-
-module.exports = app;
--- next-tailwind-pokedex/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: { 
-    domains: ['assets.pokemon.com']
-  }
-}
-
-module.exports = nextConfig
