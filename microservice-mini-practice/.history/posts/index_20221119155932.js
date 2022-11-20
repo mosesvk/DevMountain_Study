5d36d593f7bf73379c2ce3b2951313f8582b9045
mosesvk <kaumatule93@gmail.com>
2022-11-19 17:38:29 -0700

    update

--- microservice-mini-practice/.history/posts/index_20221119155932.js
+++ microservice-mini-practice/.history/posts/index_20221119155932.js
@@ -41,6 +41,6 @@ app.post("/events", (req, res) => {
 });
 
 app.listen(4000, () => {
-  console.log('Updated')
+  console.log('updated')
   console.log("Listening on 4000");
 });
--- microservice-mini-practice/.history/posts/index_20221119160205.js
+++ microservice-mini-practice/.history/posts/index_20221119160205.js
@@ -41,6 +41,6 @@ app.post("/events", (req, res) => {
 });
 
 app.listen(4000, () => {
-  console.log('Updated')
+  console.log('UPDATED')
   console.log("Listening on 4000");
 });
