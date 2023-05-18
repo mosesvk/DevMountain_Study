d5c1ab3ac355ef1acecc9c641bf725e0e800c6e9
mosesvk <kaumatule93@gmail.com>
2023-05-18 14:38:48 -0600

    handling errors

---
+++ javascript/junior/dg_beginner_course/.history/index_20230518143656.js
@@ -0,0 +1,24 @@
+// JS Errors & Error Handling
+
+const makeError = () => {
+  let i = 1;
+  while (i <= 5) {
+    try {
+      if (i % 2 !== 0) throw new Error('odd number!');
+      console.log('even number');
+    } catch (err) { // this will execute ONLY if Error occurs
+      console.error(err.stack);
+    } finally { // this will execute no matter what 
+      console.log('....finally');
+      i++;
+    }
+  }
+}
+
+makeError();
+
+function customError(message) {
+  this.message = message;
+  this.name = 'customError';
+  this.stack = `${this.name}: ${this.message}`;
+}
