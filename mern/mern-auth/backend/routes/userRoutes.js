f477f14883c7c364c2491835fcbbc97357b10c47
mosesvk <kaumatule93@gmail.com>
2023-05-25 12:00:13 -0600

    START HERE - Beginning Backend setup along with packages

--- mern/mern-auth/backend/routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-import { authUser } from '../controllers/userController.js'
-const router = express.Router()
-
-router.post('/users/auth', authUser)
-
-export default router
