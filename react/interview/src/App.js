96f456a8c32ac607b51ff83d4a37df142e5aec07
mosesvk <kaumatule93@gmail.com>
2023-05-29 12:00:29 -0600

    udpate

--- react/interview/src/App.js
+++ react/interview/src/App.js
@@ -4,17 +4,22 @@ import axios from 'axios';
 // https://jsonplaceholder.typicode.com/users
 
 const fetchApi = async () => {
-  const {data} = await axios.get('https://jsonplaceholder.typicode.com/users')
-  console.log(data)
-  return JSON.stringify(data, null, 2)
+  const {data} = await axios.get('https://randomuser.me/api')
+  console.log(data.results)
+  return JSON.stringify(data.results, null, 2)
 }
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [dataImg, setDataImg] = useState('')
 
   const buttonApiCall = async () => {
     const data = await fetchApi()
     setUsers(data)
+
+
   }
   
   return (<div>
