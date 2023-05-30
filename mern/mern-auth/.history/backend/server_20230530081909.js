ed5eb780152efd0496a2afef16b47addc287dcc0
mosesvk <kaumatule93@gmail.com>
2023-05-30 08:36:38 -0600

    START HERE Test-3
    Here we connect our application to mongoose

--- mern/mern-auth/.history/backend/server_20230530081909.js
+++ mern/mern-auth/.history/backend/server_20230530081909.js
@@ -2,8 +2,11 @@ import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
 import {notFound, errorHandler} from './middleware/errorHandler.js'
+import connectDB from './config/db.js'
 const port = process.env.PORT 
 import userRoutes from './routes/userRoutes.js'
+
+// connectDB()
 
 const app = express()
 
