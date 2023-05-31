cbd692726e599d77528515a87df74d559159e41b
mosesvk <kaumatule93@gmail.com>
2023-05-31 09:42:41 -0600

    completed test3

--- react/interview/src/App.js
+++ react/interview/src/App.js
@@ -1,6 +1,5 @@
-import { useState, useEffect } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import axios from 'axios';
-import Card from 'react-bootstrap/Card';
 
 //https://jsonplaceholder.typicode.com/users
 
@@ -11,34 +10,58 @@ const fetchApi = async () => {
   return JSON.stringify(data, null, 2);
 };
 
-const CardComponent = ({user}) => {
+const CardComponent = ({ user }) => {
   return (
-    <div className='card' key={user.id}>
+    <div className='card'>
       <h2>{user.name}</h2>
       <h3>{user.username}</h3>
     </div>
   );
 };
 
+const NotFoundComponent = () => {
+  return (
+    <div className='card'>
+      <h1>No Users Found</h1>
+    </div>
+  );
+};
+
 const App = () => {
   const [usersArray, setUsersArray] = useState([]);
+  const inputRef = useRef();
+
+  useEffect(() => {
+    fetchApi()
+      .then((res) => JSON.parse(res))
+      .then((json) => setUsersArray(json));
+  }, []);
 
   const clickHandler = async () => {
     const data = await fetchApi();
     setUsersArray(JSON.parse(data));
   };
 
-  // const filteredUsersArray = usersArray.filter(user => (
-
-  // ))
+  const filteredUsersArray = usersArray.filter((user) => {
+    let name = user.name;
+    let string = inputRef.current.value;
+    return name.toLowerCase().startsWith(string);
+  });
 
   return (
     <div className='App'>
-      <button onClick={clickHandler}>fetch api</button>
+      <div className='header'>
+        <input type='text' ref={inputRef} />
+        <button onClick={clickHandler}>fetch api</button>
+      </div>
       <div className='card-div'>
-        {usersArray.map((user) => (
-          <CardComponent user={user} />
-        ))}
+        {filteredUsersArray &&
+          filteredUsersArray.map((user) => (
+            <CardComponent user={user} key={user.id} />
+          ))}
+        {!filteredUsersArray.length && inputRef.current && (
+          <NotFoundComponent />
+        )}
       </div>
     </div>
   );
