36dfa377e091a80499f6250b460864e328a6da6a
mosesvk <kaumatule93@gmail.com>
2023-09-12 15:11:19 -0600

    update

--- react/react-redux/05_state/.history/animals/src/App_20230912131745.js
+++ react/react-redux/05_state/.history/animals/src/App_20230912131745.js
@@ -3,13 +3,6 @@ import { useState } from 'react';
 function App() {
   const [count, setCount] = useState(0);
 
-  const getAnimal = () => {
-    const animals = ['bird', 'cat', 'cow', 'dog', 'horse']
-
-    return animals[Math.floor(Math.random() * (animals.length - 1))]
-
-  }
-
   const handleClick = () => {
     setCount(count + 1);
   };
