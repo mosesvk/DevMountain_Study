4dc3fee2b830e04076ce6800ca6a0e3569968ed2
mosesvk <kaumatule93@gmail.com>
2023-09-20 15:52:39 -0600

    update

---
+++ javascript/basic-projects/.history/new/loops/app_20230920150024.js
@@ -0,0 +1,23 @@
+// Get all the menu anchor tags
+const menuLinks = document.querySelectorAll('.menu-link');
+
+// // Loop through the array of menu links and add dynamic classes
+// menuLinks.forEach((link, index) => {
+//     if (index === 0) {
+//         link.classList.add('first');
+//     } else if (index === 1) {
+//         link.classList.add('second');
+//     } else if (index === 2) {
+//         link.classList.add('third');
+//     }
+//     // You can continue this pattern for more positions/classes
+// });
+
+// Define the class names in an array
+const classNames = ['first', 'second', 'third', 'fourth'];
+
+// Loop through the array of menu links and add dynamic classes
+menuLinks.forEach((link, index) => {
+  const classNameIndex = index % classNames.length;
+  link.classList.add(classNames[classNameIndex]);
+});
