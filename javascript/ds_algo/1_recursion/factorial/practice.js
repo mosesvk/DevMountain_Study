d6f2f879f832410cfa52e229804d05a0a1f1c4e2
mosesvk <kaumatule93@gmail.com>
2023-10-16 11:29:07 -0600

    recursion

---
+++ javascript/ds_algo/1_recursion/factorial/practice.js
@@ -0,0 +1,13 @@
+//factorial(1) // 1 x 1 = 1
+// factorial(2) // 2 x 1 x 1 = 2
+// factorial(4) // 4 x 3 x 2 x 1 = 24
+// factorial(7) // 7 x 6 x 5 x 4 x 3 x 2 x 1 = 5040
+
+// Write a function factorial which accepts a number and returns the factorial of that number. 
+// A factorial is the product of an integer and all the integers below it; e.g., factorial four ( 4! ) is equal to 24, because 4 * 3 * 2 * 1 equals 24.  factorial zero (0!) is always 1.
+
+
+
+function factorial(num){
+
+}
